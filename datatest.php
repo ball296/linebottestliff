f417cf274c6d52658bdad85c91a54b69fde05344
ball296 <57444299+ball296@users.noreply.github.com>
2020-12-21 23:27:39 +0700

    Update datatest.php

--- datatest.php
+++ datatest.php
@@ -8,7 +8,7 @@ while ($row = mysqli_fetch_assoc($result))
 
 }
 $data2=echo json_encode($data);
-?>
+
 
 </script>
 <!DOCTYPE html>
@@ -111,3 +111,5 @@ $data2=echo json_encode($data);
   </script>
 </body>
 </html>
+?>        
+        
