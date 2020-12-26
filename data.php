ccdd67f55f137dc74a9baea610515886b16cfe9e
ball296 <57444299+ball296@users.noreply.github.com>
2020-12-26 14:32:35 +0700

    Update data.php

--- data.php
+++ data.php
@@ -11,13 +11,20 @@ while ($row = mysqli_fetch_assoc($result))
 $namepea1="namepea";
 $typevoc="Group1";
 $stw="statusvoc";
+$numvocdata="numvoc";
 $numdata=count($data);
+$numdaydata="numday";
+$namedata="namecus";
+
 for ($x = 0; $x <= $numdata; $x++) {
     if ($data[$x][$typevoc]<>"ร้องเรียน" and $data[$x][$stw]=="กำลังดำเนินการ" ){
-    $data2[]= $data[$x][$namepea1]." ".$data[$x][$stw];
+      if($data[$x][$numdaydata]<=15){
+    $data2[]= $data[$x][$namepea1]." เลขที่ร้อง ".$data[$x][$numvocdata]." ชื่อผู้ร้อง ".$data[$x][$namedata]." ร้องแล้ว ".$data[$x][$numdaydata]." วัน สถานะ "."'".$data[$x][$stw]."'" ;
+  }
   }
 }
-$newccAddress = implode('\n\n', $data2); 
+}
+#$newccAddress = implode('\n\n', $data2); 
 #print $newccAddress ;
 ?>
 
