5eddb5ddb3606f1621b7428a17ea5bc02ef7d77d
ball296 <57444299+ball296@users.noreply.github.com>
2020-12-26 23:48:17 +0700

    Update datatest.php

--- datatest.php
+++ datatest.php
@@ -1,3 +1,9 @@
+<?php 
+    include("data.php");
+    $myArray=$newccAddress;
+    date_default_timezone_set("Asia/Bangkok");
+    $datedata=date("d/m/Y");
+?>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,6 +26,23 @@
                type:"flex",
                altText:"รายงานข้อร้องเรียนประจำวัน",
                contents:{
+"type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://gdurl.com/UyRY",
+    "margin": "xxl",
+    "size": "3xl",
+    "aspectRatio": "20:5",
+    "backgroundColor": "#79058BF2"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "backgroundColor": "#F8EEEEFF",
+    "borderColor": "#DA2A14EA",
+    "contents": [
+      {
   "type": "bubble",
   "direction": "ltr",
   "header": {
@@ -33,6 +56,19 @@
         "color": "#FFFFFFF9",
         "align": "center",
         "contents": []
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "ประจำวันที่",
+            "align": "center",
+            "margin": "sm",
+            "contents": []
+          }
+        ]
       }
     ]
   },
@@ -40,45 +76,54 @@
     "type": "box",
     "layout": "vertical",
     "contents": [
-      {
-        "type": "box",
-        "layout": "vertical",
-        "backgroundColor": "#770D6EFF",
-        "contents": [
-          {
-            "type": "text",
-            "text": "1. กฟอ.แม่สอด",
-            "weight": "bold",
-            "color": "#F7EBEBFF",
-            "gravity": "center",
-            "margin": "sm",
-            "wrap": true,
-            "contents": []
+      <?php
+      $datacount=count($data2)+1;
+      #echo $datacount;
+      $colorvalue=1; 
+      foreach($data2 as $value) {
+        if ($colorvalue%2==0){$color="#770D6EFF";}else{$color="#B90CAAFF";}
+      echo "{";
+        echo '"type":"box",';
+        echo '"layout": "vertical",';
+        echo '"backgroundColor":"'.$color.'",';
+        echo '"contents": [';
+          echo "{";
+            echo '"type": "text",';
+            echo '"text": "'.$value.'",';
+            echo '"weight": "bold",';
+            echo '"color": "#F7EBEBFF",';
+            echo '"gravity": "center",';
+            echo '"margin": "sm",';
+            echo '"wrap": true,';
+            echo '"contents": []';
+          echo "}";
+        echo "]";
+        if($colorvalue==$datacount-1){
+      echo "}";}else{echo "},";}
+      $colorvalue=$colorvalue+1;
           }
-        ]
-      },
-      {
-        "type": "box",
-        "layout": "vertical",
-        "backgroundColor": "#B90CAAFF",
-        "contents": [
-          {
-            "type": "text",
-            "text": "2. กฟจ.พิษณุโลก ",
-            "weight": "bold",
-            "color": "#F7EFEFFF",
-            "margin": "sm",
-            "wrap": true,
-            "contents": []
-          }
-        ]
-      }
+      ?>
     ]
   },
   "footer": {
     "type": "box",
     "layout": "horizontal",
     "contents": [
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "แผนกลูกค้าสัมพันธ์ โทร (12) 10215",
+            "weight": "bold",
+            "size": "xs",
+            "color": "#FFFFFFFF",
+            "wrap": true,
+            "contents": []
+          }
+        ]
+      },
       {
         "type": "box",
         "layout": "horizontal",
@@ -89,7 +134,7 @@
             "action": {
               "type": "uri",
               "label": "Share",
-              "uri": "https://linecorp.com"
+              "uri": "https://liff.line.me/1655423177-8dMEraKE"
             },
             "color": "#860B8CFF",
             "margin": "md",
