4555fb4c9bccd2e9543c348b4bd4327dc9717e1d
ball296 <57444299+ball296@users.noreply.github.com>
2020-12-26 16:07:43 +0700

    Update data.php

--- data.php
+++ data.php
@@ -15,16 +15,22 @@ $numvocdata="numvoc";
 $numdata=count($data);
 $numdaydata="numday";
 $namedata="namecus";
-
+$k=1;
+$j=1;
 for ($x = 0; $x <= $numdata; $x++) {
     if ($data[$x][$typevoc]<>"ร้องเรียน" and $data[$x][$stw]=="กำลังดำเนินการ" ){
-      if($data[$x][$numdaydata]<=15){
-    $data2[]= $data[$x][$namepea1]." เลขที่ร้อง ".$data[$x][$numvocdata]." ชื่อผู้ร้อง ".$data[$x][$namedata]." ร้องแล้ว ".$data[$x][$numdaydata]." วัน สถานะ "."'".$data[$x][$stw]."'" ;
+      if($data[$x][$numdaydata]>=15){
+    $data2[]= ($k)." ".$data[$x][$namepea1]." เลขที่ร้อง ".$data[$x][$numvocdata]." ชื่อผู้ร้อง ".$data[$x][$namedata]." ร้องแล้ว ".$data[$x][$numdaydata]." วัน สถานะ "."'".$data[$x][$stw]."'" ;
+    $k=$k+1;
   }
   }
-
+    if ($data[$x][$typevoc]<>"ร้องเรียน" and $data[$x][$stw]=="รอดำเนินการ" ){ 
+    $data3[]=($j)." ".$data[$x][$namepea1]." เลขที่ร้อง ".$data[$x][$numvocdata]." ชื่อผู้ร้อง ".$data[$x][$namedata]." ร้องแล้ว ".$data[$x][$numdaydata]." วัน สถานะ "."'".$data[$x][$stw]."'" ;
+    $j=$j+1;
+    }
 }
-$newccAddress = implode('\n\n', $data2); 
+
+$newccAddress = implode('\n\n', $data3);
+$newccAddress2=implode('\n\n',$data2); 
 #print $newccAddress ;
 ?>
-
