7f7421a9a3f7e0867be7f7eac2ab5d6b3b17f6f0
ball296 <57444299+ball296@users.noreply.github.com>
2020-12-27 16:41:49 +0700

    Update datatest.php

--- datatest.php
+++ datatest.php
@@ -56,7 +56,7 @@
         "contents": [
           {
             "type": "text",
-            "text": "red",
+            "text": "'echo $data2[$i];'",
             "weight": "bold",
             "color": "#F7EBEBFF",
             "gravity": "center",
